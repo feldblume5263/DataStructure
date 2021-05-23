829a59e0175bb782cc2a0af26c088ad50f710784
feldblume5263 <feldblume5263@gmail.com>
2021-05-23 22:05:24 +0900

    update code

--- assignment2/bsTIteratior.c
+++ assignment2/bsTIteratior.c
@@ -11,14 +11,21 @@ typedef struct			s_Node
 	struct s_Node		*right;
 }						TreeNode;
 
+typedef struct			s_binary_search_tree
+{
+	TreeNode			*root;
+	int					(*cmp)(int, int);
+	int					size;
+}						Tree;
+
 typedef struct			s_Iterator
 {
 	int					*Tree;
 	int					idx;
 }						BSTIterator;
 
-int						command_count = 0;
 int						tree_count = 0;
+int						print_idx = 0;
 
 size_t		count_start(char const *s, char c, size_t idx)
 {
@@ -141,10 +148,10 @@ void					set_inorder_tree(TreeNode *root, BSTIterator *ret)
 {
 	if (root)
 	{
-		inorder(root->left, ret);
+		set_inorder_tree(root->left, ret);
 		(ret->Tree)[ret->idx] = root->content;
 		ret->idx++;
-		inorder(root->right, ret);
+		set_inorder_tree(root->right, ret);
 	}
 
 
@@ -156,39 +163,51 @@ BSTIterator				*bSTIteratorCreate(TreeNode *root)
 
 	if (!(ret->Tree = (int*)malloc(sizeof(int) * (tree_count))))
 		return (NULL);
-	ret->idx = 0; // start
+	ret->idx = 0;
 	set_inorder_tree(root, ret);
 	return (ret);
 }
 
 int						bSTIteratorNext(BSTIterator *obj)
 {
+	int					ret;
 
-
-
+	if(obj->idx)
+	{
+		ret = obj->Tree[print_idx + 1];
+		obj->idx--;
+		print_idx++;
+	}
+	else
+	{
+		ret = 1;
+		printf("]\n");
+		printf("\nERROR: Have no Next content!\n");
+		printf("\nERROR: Process over\n");
+		exit(ret);
+	}
+	return (ret);
 }
 
 bool					bSTIteratorHasNext(BSTIterator *obj)
 {
-
-
+	return (obj->idx - 1 > 0);
 }
 
 void					bSTIteratorFree(BSTIterator *obj)
 {
-
-
-
+	free(obj->Tree);
+	free(obj);
 }
 
-char					**refine_input(char **input)
+char					**parse_input(char **input)
 {
 	int					idx;
 
 	idx = 0;
 	while (idx < strlen(*input))
 	{
-		if ((*input)[idx] == '[' || (*input)[idx] == ']' || (*input)[idx] == ' ')
+		if ((*input)[idx] == '[' || (*input)[idx] == ']' || (*input)[idx] == ' ' | (*input)[idx] == '"')
 			(*input)[idx] = ',';
 		idx++;
 	}
@@ -196,60 +215,150 @@ char					**refine_input(char **input)
 }
 
 
-void					make_complete_binary_tree(char **tree_input, TreeNode *root)
+TreeNode		*create_elem(int data)
 {
-	int					idx;
-	int					count;
+	TreeNode	*node;
 
-	count = 1;
-	idx = 0;
-	while (tree_input[idx])
-	{
-		while (idx < count)
-		{
-			if (!(strlen(tree_input[idx]) == 3 && !strncmp(tree_input[idx], "null", 3)))
-			{
-				idx++;
-				continue ;
-			}
-			idx++;
-		}
-	}
+	node = malloc(sizeof(TreeNode));
+	if (!node)
+		return (0);
+	node->content = data;
+	node->left = NULL;
+	node->right = NULL;
+	return (node);
 }
 
-int						get_tree_count(char **tree_input)
+int						make_complete_binary_tree(Tree *tree, int data)
+{
+
+	TreeNode			*new;
+	TreeNode			*curr;
+	TreeNode			*prev;
+	int					cmp_result;
+
+	if (!tree)
+		return (0);
+	curr = tree->root;
+	prev = 0;
+	while (curr)
+	{
+		if (!(cmp_result = tree->cmp(data, curr->content)))
+			return (0);
+		prev = curr;
+		if (cmp_result < 0)
+			curr = curr->left;
+		else
+			curr = curr->right;
+	}
+	if (!(new = create_elem(data)))
+		return (0);
+	if (!prev)
+		tree->root = new;
+	else if (cmp_result < 0)
+		prev->left = new;
+	else
+		prev->right = new;
+	tree->size++;
+	return (1);
+}
+
+int						get_input_count(char **tree_input)
 {
 	int					count;
 
 	count = 0;
 	while (tree_input[count])
 	{
+		if (strlen(tree_input[count]) == 3 && !(strncmp(tree_input[count], "null", 3)))
+		{
+			continue ;
+		}
 		count++;
 	}
 	return (count);
 }
 
+
+Tree				*tree_init(int (*cmp)(int, int))
+{
+	Tree			*tree;
+
+	if (!cmp || !(tree = malloc(sizeof(Tree))))
+		return (0);
+	tree->root = 0;
+	tree->cmp = cmp;
+	tree->size = 0;
+	return (tree);
+}
+
+int						cmp(int a, int b)
+{
+	return (a - b);
+}
+
 int						main()
 {
-	BSTIterator			iterator;
-	TreeNode			root;
+	BSTIterator			*iterator;
+	Tree				*root;
 	char				*input1;
 	char				*input2;
 	char				**command_input;
 	char				**tree_input;
+	int					idx;
 
+	printf("Enter Commands : \n");
 	get_input(&input1);
+	printf("\nEnter Tree : \n");
 	get_input(&input2);
 
-	command_input = refine_input(&input1);
-	tree_input = refine_input(&input2);
-	tree_count = get_tree_count(tree_input);
-	make_complete_binary_tree(tree_input, &root);
+	command_input = parse_input(&input1);
+
+	tree_input = parse_input(&input2);
+	tree_count = get_input_count(tree_input);
+
+	root = tree_init(cmp);
+	idx = 0;
+	while (tree_input[idx])
+	{
+		if (strlen(tree_input[idx]) == 3 && !(strncmp(tree_input[idx], "null", 3)))
+		{
+			idx++;
+			continue ;
+		}
+		make_complete_binary_tree(root, atoi(tree_input[idx]));
+		idx++;
+	}
+
+	iterator = bSTIteratorCreate(root->root);
+	idx = 0;
+	printf("\n\nresult:\n[");
+	while (command_input[idx])
+	{
+		if (strlen(command_input[idx]) == 4 && strncmp(command_input[idx], "next", 4) == 0)
+		{
+			printf("%d", bSTIteratorNext(iterator));
+		}
+		else if (strlen(command_input[idx]) == 7 && strncmp(command_input[idx], "hasNext", 7) == 0)
+		{
+			if (bSTIteratorHasNext(iterator) == true)
+				printf("true");
+			else
+				printf("false");
+		}
+		else if (strlen(command_input[idx]) == 11 && strncmp(command_input[idx], "BSTIterator", 11) == 0)
+		{
+			printf("null");
+		}
+		if (command_input[idx + 1])
+			printf(", ");
+		idx++;
+	}
+	printf("]\n");
+
 
 	free(input1);
 	input1 = NULL;
 	free(input2);
 	input2 = NULL;
-
-
+	bSTIteratorFree(iterator);
 }
